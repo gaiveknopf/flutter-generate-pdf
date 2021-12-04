10a7f6d53d1527af0ee489dbf63a25addac209dc
gaiveknopf <avigaive@hotmail.com>
2021-12-04 16:29:20 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:share_plus/share_plus.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Gerador de PDF'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<void> generatePDF() async {
+    final pdf = pw.Document();
+
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) => pw.Center(
+          child: pw.Text('Hello World!'),
+        ),
+      ),
+    );
+
+    String dir = (await getApplicationDocumentsDirectory()).path;
+    String fullPath = '${dir}/test.pdf';
+    File file = File(fullPath);
+    await file.writeAsBytes(await pdf.save());
+    Share.share(fullPath);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Clique no botão para gerar pdf e compartilhar',
+            ),
+            ElevatedButton(
+              child: const Text('Gerar PDF e Compartilhar'),
+              onPressed: generatePDF,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
